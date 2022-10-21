dd24c80cda788ec373ced783bc9c9aea178c46a8
Onyekachi <kukiworldwideng@gmail.com>
2022-10-21 13:08:55 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-##Django_Project
+# Django_Project
+```
+This is new Django project named “songcrud” and an app in the project called “musicapp”. 
+this repo contains a requirements.txt file housing all the pinned dependencies 
+for this  project.
 
-#This is new Django project named “songcrud” and an app in the project called “musicapp”. this repo contains a requirements.txt file housing all the pinned dependencies for this  project.
+In the models.py file inside the “musicapp” application includes the following Models and Attributes.
+
+Model: Artiste, Song, Lyric
+Attributes for “Artiste” : first_name, last_name, age
+Attributes for “Song” : title, date released, likes, artiste_id
+Attributes for “Lyric”: content, song_id
+
+```
+
+
--- musicapp/models.py
+++ musicapp/models.py
@@ -1,3 +1,32 @@
+from email.policy import default
 from django.db import models
+from datetime import date
 
 # Create your models here.
+
+class Artiste (models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return(self.first_name)
+
+ 
+class Song (models.Model):
+    title = models.CharField(max_length=100)
+    date_released = models.DateField(default=date.today)
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return (self.title)
+    
+
+class Lyrics (models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return(self.song_id)
+        
